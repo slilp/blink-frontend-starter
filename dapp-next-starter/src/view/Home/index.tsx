9f8bc29ae1ce2d293e0a52e360087b862454a229
slil_puangpoom <slil.pua@MacBook-Air-khxng-slil-puangpoom.local>
2023-01-15 20:54:06 +0700

    feat : add new dapp starter

---
+++ dapp-next-starter/src/view/Home/index.tsx
@@ -0,0 +1,25 @@
+import CoinbaseWalletCard from "@/components/connectorCards/CoinbaseWalletCard";
+import GnosisSafeCard from "@/components/connectorCards/GnosisSafeCard";
+import MetaMaskCard from "@/components/connectorCards/MetaMaskCard";
+import NetworkCard from "@/components/connectorCards/NetworkCard";
+import WalletConnectCard from "@/components/connectorCards/WalletConnectCard";
+import { getName } from "@/connectors/utils";
+import { useWeb3React } from "@web3-react/core";
+import React from "react";
+
+function Home() {
+  const { connector, chainId, isActive } = useWeb3React();
+  return (
+    <div>
+      <main className="mx-auto container p-10 ">
+        <div className="flex justify-center">
+          <MetaMaskCard />
+          <WalletConnectCard />
+          <NetworkCard />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
